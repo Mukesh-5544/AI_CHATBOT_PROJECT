eb6e9b185371b2fa0686dcabec6745e1c9b2e62a
Mukesh Togaru <mukeshtogaru@gmail.com>
2026-02-16 08:25:28 +0530

    Added Render PORT config

--- app.py
+++ app.py
@@ -37,5 +37,10 @@ def chatbot():
     response = chatbot_response(user_input)
     return jsonify({"response": response})
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
